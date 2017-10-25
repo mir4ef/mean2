ab7dda4a6081bd9b024db6aae24bfc55649aaeec
Miroslav Georgiev <mir4ef@gmail.com>
2017-10-25 00:50:18 -0400

    importing the Observable throw method static definition

--- src/app/core/http/core-http.service.ts
+++ src/app/core/http/core-http.service.ts
@@ -8,6 +8,7 @@ import {
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
